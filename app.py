6806569c2193f25c539f174cc747ff7a8155864d
simonevincenzi <simon.vincenz@gmail.com>
2016-01-02 16:14:58 -0800

    index error commit

--- app.py
+++ app.py
@@ -97,9 +97,7 @@ def index():
             if app.volume != False:
                 p1.line(dates, volume,line_width=2, color="red",legend="Volume",)
                 p1.yaxis.axis_label = 'Shares'
-          	if app.opening_price != False:
-            	p1.line(dates, opening_prices,line_width=2, color="blue", legend="Opening price")
-            	p1.yaxis.axis_label = '$
+          	
             
         
             plots = {'Red': p1}
