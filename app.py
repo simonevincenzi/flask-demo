d9ba1c3dcb0ec962a1df37feffb6d701b5116fcb
simonevincenzi <simon.vincenz@gmail.com>
2016-01-02 17:33:14 -0800

    daily diff price commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, Markup
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show # standard bokeh import
 from bokeh.models import Range1d
 from bokeh.embed import components
 import time
 import datetime
 import requests
 import simplejson as json
-import numpy as np
+import numpy as np 
 import pandas as pd
 
 app = Flask(__name__)
@@ -28,6 +28,7 @@ def index():
         app.closing_price = ''
         app.volume = ''
      	app.opening_price = ''
+     	app.daily_diff_price = ''
         
         return render_template('index.html') # this displays the form for data entry
     else:
@@ -47,59 +48,71 @@ def index():
             app.opening_price = request.form['opening_price']
         else:
             app.opening_price = False
+        
+        if request.form.get("daily_diff_price"):
+            app.daily_diff_price = request.form['daily_diff_price']
+        else:
+            app.daily_diff_price = False
+            
 
-        if app.volume == False and app.closing_price == False and app.opening_price == False:
+        if app.volume == False and app.closing_price == False and app.opening_price == False and app.daily_diff_price == False:
             app.script = ''
             app.div = ''
             
-            app.msg = 'No options selected for display. Select either volume, opening, or closing prices'
+            app.msg = 'No options selected for display. Select either volume, opening, closing prices, or closing - opening'
             return render_template('error_page.html', msg = app.msg)
             
-        mydata = requests.get("https://www.quandl.com/api/v3/datasets/WIKI/"+app.stock_symbol+".json?rows=60")
+        stock_data = requests.get("https://www.quandl.com/api/v3/datasets/WIKI/"+app.stock_symbol+".json?rows=60") # last 60 days of open-market data
         
-        if 'quandl_error' in mydata.json(): # if error is returned from the query
+        if 'quandl_error' in stock_data.json(): # when the stock symbol is not correct, a field of stock_data.json is quandl_error
             app.script = ''
             app.div = ''
             
             app.msg = 'Stock symbol "' + app.stock_symbol + '" invalid. Try again'
             return render_template('error_page.html', msg = app.msg) # shows the error page
             
+         ## the problem is that occasionally the query comes back empty, I do not know how to test for it because locally it does not happen   
+            
         else:
-            df = pd.DataFrame(mydata.json()) # create pandas dataframe from mydata.json
+            stock_df = pd.DataFrame(stock_data.json()) # create pandas dataframe from stock_data.json
             #ind_date = [index for (index, x) in enumerate(df.ix['column_names'].dataset) if x == 'Date']
             #ind_close = [index for (index, x) in enumerate(df.ix['column_names'].dataset) if x == 'Close']
             #ind_open = [index for (index, x) in enumerate(df.ix['column_names'].dataset) if x == 'Open']
             #ind_vol = [index for (index, x) in enumerate(df.ix['column_names'].dataset) if x == 'Volume']
+            # for some reason the indexes are not kept, troubling
             ind_date = 0
             ind_close = 4
             ind_open = 1
             ind_vol = 5
-            #dates=pd.to_datetime(np.array(df.ix['data'][0])[:,int(ind_date[0])]) #extract the dates (position 0)
-            dates=pd.to_datetime(np.array(df.ix['data'][0])[:,ind_date]) #extract the dates (position 0)
-            closing_prices = (np.array(df.ix['data'][0])[:,ind_close]).astype(float) #extract closing_prices
-            volume = (np.array(df.ix['data'][0])[:,ind_vol]).astype(float)
-            opening_prices = (np.array(df.ix['data'][0])[:,ind_open]).astype(float)
-            app.stock_name = df['dataset']['name']
+            #dates=pd.to_datetime(np.array(stock_df.ix['data'][0])[:,int(ind_date[0])]) #extract the dates (position 0)
+            dates=pd.to_datetime(np.array(stock_df.ix['data'][0])[:,ind_date]) #extract the dates (position 0)
+            closing_prices = (np.array(stock_df.ix['data'][0])[:,ind_close]).astype(float) #extract closing_prices
+            volume = (np.array(stock_df.ix['data'][0])[:,ind_vol]).astype(float) # should be integer, but no problem
+            opening_prices = (np.array(stock_df.ix['data'][0])[:,ind_open]).astype(float)
+            diff_prices = closing_prices - opening_prices
+            app.stock_name = stock_df['dataset']['name']
             extra_text_index = app.stock_name.find("Prices, Dividends, Splits and Trading Volume")
             if extra_text_index != -1:
                 app.stock_name = app.stock_name[0:extra_text_index-1]
             
-            ##################################################
-            ########Bokeh block##############################
+            ######## Plot with Bokeh ##############################
         
             # select the tools we want
-            TOOLS="pan,wheel_zoom,box_zoom,reset,save"
+            TOOLS="pan,wheel_zoom,box_zoom,reset,save" # it would be nice to have the hover option, maybe later
         
             p1 = figure(tools=TOOLS, plot_width=600, plot_height=600, x_axis_type="datetime", x_axis_label='Date')
-            if app.opening_price != False:
+            if app.opening_price != False:  # it is never set = T
           		p1.yaxis.axis_label = '$'
           		p1.line(dates, opening_prices, line_width = 3, color = "green", legend = "Opening price")
             if app.closing_price != False:
                 p1.line(dates, closing_prices,line_width=3, color="blue", legend="Closing price")
                 p1.yaxis.axis_label = '$'
             if app.volume != False:
-                p1.line(dates, volume,line_width=3, color="red",legend="Volume",)
+                p1.line(dates, volume,line_width=3, color="brown",legend="Volume",)
                 p1.yaxis.axis_label = 'Shares'
+            if app.daily_diff_price != False:
+                p1.line(dates, diff_prices,line_width=3, color="brown",legend="Volume",)
+                p1.yaxis.axis_label = '$'
           	
             
         
@@ -111,13 +124,13 @@ def index():
             ##################################################
             ##################################################
 
-            return redirect('/graph_page') 
+            return redirect('/graph_page') # go to graph page for the plots
             
 @app.route('/graph_page')
 def graph_page():
     return render_template('graph.html',stock_symbol = app.stock_symbol, 
     closing_price = app.closing_price,opening_price = app.opening_price,
-    volume = app.volume,stock_name = app.stock_name, scr = Markup(app.script), diiv = Markup(app.div))
+    volume = app.volume, daily_diff_price = app.daily_diff_price,stock_name = app.stock_name, scr = Markup(app.script), diiv = Markup(app.div))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
